11b293ff498abecbe358ed26f73b470d08270085
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-02-10 15:50:05 +0200

    solve add solution

---
+++ src/main/kotlin/Add.kt
@@ -0,0 +1,8 @@
+//https://app.codesignal.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa
+fun solution(param1: Int, param2: Int): Int {
+    return param1 + param2
+}
+
+fun main() {
+println( solution(6,15))
+}
