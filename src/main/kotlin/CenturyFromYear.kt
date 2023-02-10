27864094d5caedef1252f5b01d09311dbadfd73d
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-02-10 16:00:00 +0200

    solve CenturyFromYear solution

--- src/main/kotlin/CenturyFromYear.kt
+++ src/main/kotlin/CenturyFromYear.kt
@@ -1,10 +1,11 @@
+//https://app.codesignal.com/arcade/intro/level-1/egbueTZRRL5Mm4TXN
 object CenturyFromYear {
     fun solution(year: Int): Int {
-       return (year+99) / 100
+        return (year + 99) / 100
     }
 
 }
 
 fun main() {
-println( CenturyFromYear.solution(2000))
+    println(CenturyFromYear.solution(2000))
 }
