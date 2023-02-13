23e0509b9d3177648128463b305585c0079b9a5a
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-02-13 19:01:25 +0200

    update solutions

--- src/main/kotlin/TheChanceFirstChallenge.kt
+++ src/main/kotlin/TheChanceFirstChallenge.kt
@@ -1,17 +1,15 @@
 //https://app.codesignal.com/challenge/JJfSLsWMTsFcqFEXS
 object TheChanceFirstChallenge {
     fun solution(l: MutableList<String>): Double {
-        var A = 0
-        var B = 0
-        for (i in l) when (i) {
-            "a" -> A++
-            "b" -> B++
+        if (l.size < 2) return -1.0
+        var countA = 0
+        for (element in l) {
+            if (element != "a" && element != "b") return -1.0
+            if (element == "a") countA++
         }
-        if (A != 0 && B != 0 && !l.any { it != "a" && it != "b" }) {
-            return A.toDouble() / l.size.toDouble()
-        }
-        return -1.0
+        return countA.toDouble() / l.size
     }
+
 }
 
 fun main() {
@@ -27,3 +25,15 @@ fun main() {
 }
 
 //fun solution(s: List<*>)= if(s.toSet() == setOf("a" , "b")) s.count{it=="a"}*1F/s.size else -1
+/*fun solution(l: MutableList<String>): Double {
+        var A = 0
+        var B = 0
+        for (i in l) when (i) {
+            "a" -> A++
+            "b" -> B++
+        }
+        if (A != 0 && B != 0 && !l.any { it != "a" && it != "b" }) {
+            return A.toDouble() / l.size.toDouble()
+        }
+        return -1.0
+}*/
