2a4c545e8443245f4b9e737f16e9e2bc6188acd5
Abanoub-Nagy <abanoubnagy24@gmail.com>
2023-02-14 03:28:54 +0200

    update solutions

--- src/main/kotlin/TheChanceFirstChallenge.kt
+++ src/main/kotlin/TheChanceFirstChallenge.kt
@@ -2,12 +2,12 @@
 object TheChanceFirstChallenge {
     fun solution(l: MutableList<String>): Double {
         if (l.size < 2) return -1.0
-        var countA = 0
-        for (element in l) {
-            if (element != "a" && element != "b") return -1.0
-            if (element == "a") countA++
+        var A = 0
+        for (i in l) {
+            if (i != "a" && i != "b") return -1.0
+            if (i == "a") A++
         }
-        return countA.toDouble() / l.size
+        return A.toDouble() / l.size
     }
 
 }
@@ -24,7 +24,7 @@ fun main() {
     )
 }
 
-//fun solution(s: List<*>)= if(s.toSet() == setOf("a" , "b")) s.count{it=="a"}*1F/s.size else -1
+
 /*fun solution(l: MutableList<String>): Double {
         var A = 0
         var B = 0
@@ -36,4 +36,20 @@ fun main() {
             return A.toDouble() / l.size.toDouble()
         }
         return -1.0
+
+        *****************************************
+
+        val A = l.count { it.toLowerCase() == "a" }
+        val B = l.count { it.toLowerCase() == "b" }
+        return when {
+            A == 0 || B == 0 -> -1.0
+            else -> A.toDouble() / l.size
+        }
+
+        *******************************
+        return if (l.any { it.toLowerCase() != "a" && it.toLowerCase() != "b" } || l.size < 2) -1.0
+        else l.count { it.toLowerCase() == "a" } / l.size.toDouble()
+
+
+        //fun solution(s: List<*>)= if(s.toSet() == setOf("a" , "b")) s.count{it=="a"}*1F/s.size else -1
 }*/
